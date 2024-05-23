d11e2ec6f48abdbf757c809536d20141329be616
Muellol7 <muellol7@students.zhaw.ch>
2024-05-23 17:40:53 +0200

    6. Funktion erweitern, dass wenn keine Zahl mitgegeben wird, die Funktion 0 zurückgibt

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/StringCalculator.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/StringCalculator.java
@@ -2,7 +2,11 @@ package ch.zhaw.iwi.devops.demo;
 
 public class StringCalculator {
     public int add (String numbers) {
-      
+        
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        
         String[] numberArray = numbers.split(",");
         int sum = 0;
         
