7010a72c2e398b0136f4522324af96c62f93f0c5
Muellol7 <muellol7@students.zhaw.ch>
2024-05-23 18:24:40 +0200

    17.	Test 6: Negative Zahlen werden nicht erlaubt

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/StringCalculatorTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/StringCalculatorTest.java
@@ -1,5 +1,6 @@
 package ch.zhaw.iwi.devops.demo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +16,11 @@ public class StringCalculatorTest {
         assertEquals(15, calculator.add("1,2,3,4,5"));
         assertEquals(6, calculator.add("1\n2,3"));
         assertEquals(5, calculator.add("//;\n2;3"));
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            calculator.add("1,-2,3,-4");
+        });
+        assertEquals("Negatives not allowed: [-2, -4]", exception.getMessage());
+
     }
 }
